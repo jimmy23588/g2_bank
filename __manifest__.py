f5b3e70d77a1f6bffd84a6af355f8380fb39167b
jimmy <jimmy@max12>
2025-12-05 13:58:41 +0100

    Primer Commit

--- __manifest__.py
+++ __manifest__.py
@@ -1,16 +1,18 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "g2_bank",
+    'name': "BankOdoo",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
         subtitle on modules listing or apps.openerp.com""",
 
     'description': """
-        Long description of module's purpose
+        Open Accounts and  Movements :
+             - Consult Account
+             - Consult Movemnt
     """,
 
-    'author': "My Company",
+    'author': "Grupo2Dam",
     'website': "https://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
--- models/movement.py
+++ models/movement.py
@@ -3,11 +3,11 @@
  from odoo import models, fields, api
 
 
- class g2_bank(models.Model):
-#     _name = 'g2_bank.g2_bank'
-#     _description = 'g2_bank.g2_bank'
+ class Movement(models.Model):
+     _name = 'g2_bank.movement'
+     _description = 'Movement'
 
-#     name = fields.Char()
+     name = fields.Char()
 #     value = fields.Integer()
 #     value2 = fields.Float(compute="_value_pc", store=True)
 #     description = fields.Text()
