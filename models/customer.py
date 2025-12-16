088f7427835d529716bcaaf4a08798a29341c1dc
VersatileAnerst <alarconbasc@gmail.com>
2025-12-16 14:02:37 +0100

    customer inherit

--- models/customer.py
+++ models/customer.py
@@ -3,12 +3,14 @@
  from odoo import models, fields, api
 
 
- class Movement(models.Model):
-     _name = 'g2_bank.movement'
-     _description = 'Movement'
+ class Customer(models.Model):
+     _name = 'g2_bank.Customer'
+     _description = 'Customer'
+     _inherit = 'res.users'
 
-     name = fields.Char()
      
+     Accounts_ids = fields.Many2Many('g2_bank.Customer', string="Customers")
+#     name = fields.Char()
 #     value = fields.Integer()
 #     value2 = fields.Float(compute="_value_pc", store=True)
 #     description = fields.Text()
