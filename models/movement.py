1ea5955153845b2ad0bce5f33a9e22125cd85c32
jimmy <jimmy@max12>
2025-12-16 14:11:54 +0100

    pri

--- models/movement.py
+++ models/movement.py
@@ -1,13 +1,23 @@
 # -*- coding: utf-8 -*-
 
- from odoo import models, fields, api
+from odoo import models, fields, api
 
 
- class Movement(models.Model):
-     _name = 'g2_bank.movement'
-     _description = 'Movement'
+class Movement(models.Model):
+     _name = 'g2_bank.Movement'
+     _description = 'Movement of Account'
+
+     
 
      name = fields.Char()
+     timestamp = fields.Date()
+     amount = fields.Monetary()
+     balance = fields.Monetary()
+     
+     Movement_ids=fields.Many2one('g2_bank.Account', string ="Account")
+     
+     
+     
 #     value = fields.Integer()
 #     value2 = fields.Float(compute="_value_pc", store=True)
 #     description = fields.Text()
