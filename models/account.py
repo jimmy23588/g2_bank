f636eb05ea82f484b9e8c26ac17e4edadafafa33
VersatileAnerst <alarconbasc@gmail.com>
2025-12-16 10:03:08 +0100

    Primer commit

---
+++ models/account.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+ from odoo import models, fields, api
+
+
+ class Account(models.Model):
+     _name = 'g2_bank.Account'
+     _description = 'Account'
+     
+     name = fields.Text()
+     balance = fields.Monetary(required = true)
+     creditLine = fields.Monetary()
+     beginBalance = fields.Monetary()
+     beginBalanceTimestamp = fields.Datetime()
+     typeAccount = fields.Selection(required = true)
+     Customer_ids = fields.Many2Many('', string="Customers")
+     Movement_id = fields.One2many('', string="Movements")
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
