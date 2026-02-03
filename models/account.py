d6e9deaaffc2504b88116f9dc8434b0dc3747fa6
VersatileAnerst <alarconbasc@gmail.com>
2026-02-03 14:17:10 +0100

    modules cambio account

--- models/account.py
+++ models/account.py
@@ -24,4 +24,15 @@ class Account(models.Model):
      Customers_ids = fields.Many2many('res.users', string="Customers")
      Movements_ids = fields.One2many('g2_bank.movement', 'account_id', string="Movements")
 
-
+     
+def write(self, vals):
+        for record in self:
+            if record.typeAccount != 'credit':
+                # Solo editable si es credit
+                not_editable = [f for f in vals if f in ('creditLine')]
+                if not_editable:
+                    raise ValidationError(
+                        "Credit Line es editable si la cuenta es de tipo CREDIT."
+                    )
+        return super(Account, self).write(vals)
+    
