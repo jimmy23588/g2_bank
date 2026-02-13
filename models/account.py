c9673695a578d8ba0487cc6565b52440108f026b
Javier Martin Uria <javier.martinuria@educa.madrid.org>
2026-02-13 18:47:18 +0100

    FIXMEs after code inspection

--- models/account.py
+++ models/account.py
@@ -12,6 +12,7 @@ class Account(models.Model):
      balance = fields.Float(string = "Balance")
      creditLine = fields.Float(string = "Credit Line")
      beginBalance = fields.Float(string = "Begin Balance", required=True)
+	 #FIXME la fecha tiene valor por defecto la actual y no se permite editar en las vistas.
      beginBalanceTimestamp = fields.Datetime(string = "Date", required=True)
      typeAccount = fields.Selection(
      [
@@ -20,10 +21,22 @@ class Account(models.Model):
         ],
         string="Account Type"
     )
-    
+
+  	#FIXME Asocia la cuenta con el usuario que está creando la cuenta cuando esta se cree
      Customers_ids = fields.Many2many('res.users', string="Customers", required=True)
      Movements_ids = fields.One2many('g2_bank.movement', 'account_id', string="Movements")
 
+	#FIXME Añadir constrains que validen que todos los importes son positivos
+	
+	#FIXME El balance no es editable nunca (comprobarlo en la vista correspondiente) si no que 
+	#es igual al beginBalance cuando se crea la cuenta y luego se va actualizando con cada movimiento.
+	#FIXME Para lo anterior probablemente necesites redefinir un método 
+	#@api.model_create_multi
+    #def create(self, vals_list):
+        ## lógica antes de crear
+    #    records = super().create(vals_list)
+        ## lógica después de crear
+    #    return records
     
     @api.constrains('creditLine', 'typeAccount')
     def _established_credit_line_only_credit(self):
@@ -32,6 +45,8 @@ class Account(models.Model):
                 raise ValidationError(
                     "Credit Line solo puede modificarse si la cuenta es de tipo CREDIT."
                 )
+
+#FIXME Sangrado o tabulación y verifica que no se pueden borrar las cuentas con movimientos.
 def unlink(self):
         for account in self:
             if account.Movements_ids:
